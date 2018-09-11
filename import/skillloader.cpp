de9ed2b40c338de69523152ab293224d0e1eb7ce
Aditya Mehra <Aix.m@outlook.com>
2018-09-11 23:21:25 +0930

    remove unused dir stuff

--- import/skillloader.cpp
+++ import/skillloader.cpp
@@ -16,13 +16,10 @@ SkillLoader::~SkillLoader()
 
 QString SkillLoader::uiForMetadataType(const QString &metaDataType) const
 {
-    const QStringList &dirs = QStandardPaths::locateAll(QStandardPaths::GenericDataLocation, "mycroft-gui", QStandardPaths::LocateDirectory);
-    for(const QString &dir: dirs) {
-        const QString filePath = "/opt/mycroft/skills/ui/skills/"+ metaDataType + "/Main.qml";
-        qDebug() << "query" << filePath;
-        if (QFileInfo::exists(filePath)) {
-            return  QUrl::fromLocalFile(filePath).toString();
-        }
+    const QString filePath = "/opt/mycroft/skills/ui/skills/"+ metaDataType + "/Main.qml";
+    qDebug() << "query" << filePath;
+    if (QFileInfo::exists(filePath)) {
+        return  QUrl::fromLocalFile(filePath).toString();
     }
     qWarning() << "could not find delegate for " << metaDataType;
     return QString();
